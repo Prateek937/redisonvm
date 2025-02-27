8e7adbe20db450f9170cba07790c7dab3d39b73b
Prateek Sanodiya <prateeksanodiya937@gmail.com>
2025-02-27 19:07:47 +0530

    .....

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,9 +36,9 @@ resource "aws_security_group" "redis_sg" {
   description = "Security group for Redis server"
 
   ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
